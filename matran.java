785a4cd239985d5abcce2700d8aaacf1622e1db5
NguyenTrungHieu <21020017@vnu.edu.vn>
2022-06-16 09:46:16 +0700

    New Commit

---
+++ matran.java
@@ -0,0 +1,20 @@
+
+public class matran {
+    public
+    long[][] matrix = new long[2][2];
+    public matran(){
+        matrix[0][0] = 0L;
+        matrix[0][1] = 1L;
+        matrix[1][0] = 1L;
+        matrix[1][1] = 1L;
+    }
+    public void print(){
+        for(int i=0;i<2;i++){
+            System.out.print("[ ");
+            for(int j=0;j<2;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("]");
+        }
+    }
+}
---
+++ newJava.java
@@ -0,0 +1,29 @@
+public class newJava {
+    final long mod = 1000007;
+    public matran multiplyMatrix(matran a, matran b) {
+        matran c = new matran();
+        for(int i=0 ;i < 2; i++){
+            for(int j=0 ;j < 2; j++){
+                c.matrix[i][j] = 0L;
+                for(int k=0;k<2;k++){
+                    c.matrix[i][j] = (c.matrix[i][j] + a.matrix[i][k]*b.matrix[k][j] + mod*mod)%mod;   
+                }
+                //System.out.println(c.matrix[i][j]);
+            }
+        }
+        return c;
+    }
+    public matran powerMatrix(matran a, int n){
+        if(n == 1)return a;
+        matran tmp = powerMatrix(a, n/2);
+        if(n%2 == 0)return multiplyMatrix(tmp, tmp);
+        else return multiplyMatrix(multiplyMatrix(tmp, tmp), a);
+    }
+    public static void main(String[] args) {
+        newJava variable = new newJava();
+        matran a = new matran();
+        matran ab = variable.powerMatrix(a, 19411);
+        System.out.println(ab.matrix[0][1]);
+        //ab.print();
+    }
+};
